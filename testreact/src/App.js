b695cb2a21f1032708a458b02a0e53c8e350c250
DaJung Park <qkrewkjd0430@gmail.com>
2022-01-23 19:46:10 +0900

    mod.ex1

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+/*
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name ='리액트';
+  const style= {
+    //background-color는 backgroudColor와 같이 -가 사라지고 카멜표기법으로 작성
+    backgroundColor: 'black',
+    color:'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 //px  생략시에는 default 로 지정됨 
+  }
+  return <div style={style}>{name}</div>;
 }
+*/
+
+// style 미리 선언 안하고 싶을때
+/*
+function App() {
+  const name ='리액트';
+   
+  return <div style={{
+    //background-color는 backgroudColor와 같이 -가 사라지고 카멜표기법으로 작성
+    backgroundColor: 'black',
+    color:'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 //px  생략시에는 default 로 지정됨 
+  }}>{name}</div>;
+}
+*/
+function App() {
+  const name = '리액트';
+
+  return <div className="react">{name}</div>;
+}
+
 
 export default App;
